c7b056979276d094704d3aae2b651507d5b4cfe4
AvBorikDeveloper <avboris28@gmail.com>
2020-03-30 20:05:36 +1300

    Added Login activity and Twitter users pages Added twitter pictures Started do the main activity page

--- app/src/main/java/com/avborik28/twitterclone/MainActivity.java
+++ app/src/main/java/com/avborik28/twitterclone/MainActivity.java
@@ -3,15 +3,22 @@ package com.avborik28.twitterclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.parse.ParseInstallation;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtEmail, edtUsername, edtPassword;
+    private Button btnSignUp, btnLogIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ParseInstallation.getCurrentInstallation().saveInBackground();
+        setTitle("Sign Up");
     }
 }
---
+++ app/src/main/java/com/avborik28/twitterclone/TwitterUsers.java
@@ -0,0 +1,14 @@
+package com.avborik28.twitterclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwitterUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_users);
+    }
+}
